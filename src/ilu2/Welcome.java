85845cdbdd552ed8cd7cdad4c9921bcca38aed87
Corentin <corentin.ludwig@laposte.net>
2023-04-27 18:13:26 +0200

    ex4 2 nom

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -6,16 +6,20 @@ public class Welcome {
 	public static String welcome(String input) {
 		if(input.isBlank())
 			return "Hello, my friend";
-		String tabinput[] = input.split(",");
 		
+		String[] tabinput = input.split(",");
 		StringBuilder wel = new StringBuilder();
 		wel.append("Hello, ");
-		for(int = 0; i<)
-		wel.append(input.substring(0, 1).toUpperCase());
-		wel.append(input.substring(1));
+		
+		for(int i = 0; i<tabinput.length; i++) {
+			wel.append(tabinput[i].substring(0, 1).toUpperCase());
+			wel.append(tabinput[i].substring(1));
+			if(i<tabinput.length-1)
+				wel.append(", ");
+		}
 		if(input.equals(input.toUpperCase())){
 			wel.append(" !");
-			return wel.toString().toUpperCase();
+		return wel.toString().toUpperCase();
 		}
 		return wel.toString();
 	}
