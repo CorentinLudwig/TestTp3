03ab94807d46df55c58929f62ffb76f0ded76fb6
Corentin <corentin.ludwig@laposte.net>
2023-05-04 10:27:27 +0200

    exo7 insertAnd

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -6,35 +6,46 @@ public class Welcome {
 	public static String welcome(String input) {
 		if(input.isBlank())
 			return "Hello, my friend";
-		
+
 		String[] tabinput = input.split(",");
-		StringBuilder wel = new StringBuilder();
+		StringBuilder lower = new StringBuilder();
 		StringBuilder upper = new StringBuilder();
-		wel.append("Hello");
+		lower.append("Hello");
 		upper.append("HELLO");
-				
+		
+		
 		for(int i = 0; i<tabinput.length; i++) {
 			if(tabinput[i].equals(tabinput[i].toUpperCase())){
 				upper.append(", ");
 				upper.append(tabinput[i]);
 			} else {				
-				wel.append(", ");
-				wel.append(tabinput[i].substring(0, 1).toUpperCase());
-				wel.append(tabinput[i].substring(1));
+				lower.append(", ");
+				lower.append(tabinput[i].substring(0, 1).toUpperCase());
+				lower.append(tabinput[i].substring(1));
 
 				}
 		}
 		upper.append(" !");
-		if(wel.length()==5)
-			return upper.toString();
+		insertAnd(upper);
+		insertAnd(lower);
+		String upperFinal = upper.toString().toUpperCase();
+		if(lower.length()==5)
+			return upperFinal;
 		
 		if(upper.length()>7) {
-			wel.append(". AND ");
-			wel.append(upper);
+			lower.append(". AND ");
+			lower.append(upperFinal);
 		}
-		if(wel.length()==5)
-			return upper.toString();
-		return wel.toString();
+		return lower.toString();
+	}
+	
+	
+	private static StringBuilder insertAnd(StringBuilder b) {
+		int i =b.lastIndexOf(",");
+		if(i>6) {
+			b.replace(i, i+1, " and");
+		}
+		return b;
 	}
 }
 
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -30,50 +30,24 @@ class WelcomeTest {
 	
 	@Test
 	void exo4() {
-		assertEquals("Hello, Jerry, Bob", Welcome.welcome("jerry,bob"));
+		assertEquals("Hello, Jerry and Bob", Welcome.welcome("jerry,bob"));
 	}
 	
 	@Test
 	void exo5() {
-		assertEquals("Hello, Jerry, Bob, Amy", Welcome.welcome("jerry,bob,amy"));
+		assertEquals("Hello, Jerry, Bob and Amy", Welcome.welcome("jerry,bob,amy"));
 	}
 	
 	@Test
 	void exo6() {
-		assertEquals("Hello, Amy, Jerry. AND HELLO, BOB !", Welcome.welcome("Amy,BOB,Jerry"));
-		assertEquals("Hello, Amy. AND HELLO, BOB, JERRY !", Welcome.welcome("Amy,BOB,JERRY"));
+		assertEquals("Hello, Amy and Jerry. AND HELLO, BOB !", Welcome.welcome("amy,BOB,jerry"));
+		assertEquals("Hello, Amy. AND HELLO, BOB AND JERRY !", Welcome.welcome("amy,BOB,JERRY"));
 	}
 	
+	@Test
+	void exo7() {
+		assertEquals("Hello, Bob, Amy and Jerry", Welcome.welcome("bob,amy,jerry"));
+		assertEquals("Hello, Bob and Jerry. AND HELLO, AMY AND JACK !", Welcome.welcome("bob,AMY,jerry,JACK"));
+	}
 	
-//	public static String welcome(String input) {
-//	if(input.isBlank())
-//		return "Hello, my friend";
-//	
-//	String[] tabinput = input.split(",");
-//	StringBuilder wel = new StringBuilder();
-//	StringBuilder upper = new StringBuilder();
-//	wel.append("Hello, ");
-//	upper.append("HELLO, ");
-//	
-//	
-//	for(int i = 0; i<tabinput.length; i++) {
-//		if(tabinput[i].equals(tabinput[i].toUpperCase())){
-//			upper.append(tabinput[i]);
-//
-//		} else {
-//			wel.append(tabinput[i].substring(0, 1).toUpperCase());
-//			wel.append(tabinput[i].substring(1));
-//			if(i<tabinput.length-1)
-//				wel.append(", ");
-//			}
-//	}
-//	if(upper.length()>7) {
-//		upper.append(" !");
-//		wel.append(". AND ");
-//		wel.append(upper);
-//	}
-//	if(wel.length()>7)
-//		return upper.toString();
-//	return wel.toString();
-//}
 }
